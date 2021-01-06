a9a09fc667f8159bc3a5935fc9fa461a74d63396
Francisco Uribe <franciscouribematus@gmail.com>
2021-01-06 01:00:49 -0300

    Se agrego logout de knox

--- reader/urls.py
+++ reader/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+#knox
+from knox.views import LogoutView
+
 #views
 from .views import (
     ListReaderAPI,
@@ -13,6 +16,7 @@ urlpatterns = [
     path('readers', ListReaderAPI.as_view(), name='readers'),
     path('reader-register', RegisterReaderAPI.as_view(), name='reader-register'),
     path('reader-login', LoginReaderAPI.as_view(), name='reader-login'),
+    path('reader-logout', LogoutView.as_view(), name='reader-logout'),
     path('reader-profile', ListReaderAPI.as_view(), name='reader-profile'),
     path('reader-profile-update', UpdateReaderProfileAPI.as_view(), name='reader-profile-update'),
 ]
