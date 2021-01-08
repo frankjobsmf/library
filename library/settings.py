aa1ff631eb3c35e64d53ee246664e108ae1825e2
Francisco Uribe <franciscouribematus@gmail.com>
2021-01-07 21:05:24 -0300

    Configuracion MySQL

--- library/settings.py
+++ library/settings.py
@@ -89,8 +89,11 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'library',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
     }
 }
 
